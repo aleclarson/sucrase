2ea22af01f1c4b14b86ea93356ce506ca784b0bc
aleclarson <alec.stanford.larson@gmail.com>
2018-07-01 11:33:35 -0400

    remove website

--- website/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import {unregister as unregisterServiceWorker} from "./registerServiceWorker";
-import "./WorkerClient";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-unregisterServiceWorker();
